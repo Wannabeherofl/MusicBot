2e5adc1101bf376642e075aaec5320bba663eef1
Wannabeherofl <90785758+Wannabeherofl@users.noreply.github.com>
2022-03-29 22:42:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
         TOKEN: '', //write your discord bot token
         px: '!',
-        playing: 'Umut Bayraktar ❤️ Music Bot',
+        playing: 'Your Favorite songs',
 
     opt: {
         DJ: {
